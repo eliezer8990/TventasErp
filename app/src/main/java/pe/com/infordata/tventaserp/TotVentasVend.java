f504a0410e7e3eff12cedaa0ec894c3204e2874e
eliezer8990 <eliezer8990@gmail.com>
2017-07-11 14:27:03 -0500

    Updt dtpiker

--- app/src/main/java/pe/com/infordata/tventaserp/TotVentasVend.java
+++ app/src/main/java/pe/com/infordata/tventaserp/TotVentasVend.java
@@ -1,18 +1,18 @@
 package pe.com.infordata.tventaserp;
 
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
-import com.squareup.timessquare.CalendarPickerView;
+import com.leavjenn.smoothdaterangepicker.date.SmoothDateRangePickerFragment;
 
-import java.util.Calendar;
-import java.util.Date;
-
-import static com.squareup.timessquare.CalendarPickerView.SelectionMode.RANGE;
 
 public class TotVentasVend extends AppCompatActivity {
     private String array_spin1[];
@@ -22,22 +22,22 @@ public class TotVentasVend extends AppCompatActivity {
     Spinner s1;
     Spinner s2;
     Spinner s3;
-    CalendarPickerView cpv;
+    Button btnStRg;
+    String ranIni, ranFin;
+    int anioAct;
 
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tot_ventas_vend);
+        anioAct = Calendar.getInstance().get(Calendar.YEAR);
 
 
-        Date today=new Date();
-        Calendar nextYear=Calendar.getInstance();
-        nextYear.add(Calendar.YEAR,1);
-
-        cpv=(CalendarPickerView)findViewById(R.id.calendar_view);
-        cpv.init(today,nextYear.getTime()).inMode(RANGE);
 
 
+        btnStRg =(Button)findViewById(R.id.btnSetRang);
         array_spin1 = new String [3];
         array_spin1[0]="Mes y Año";
         array_spin1[1]="Año";
@@ -46,6 +46,7 @@ public class TotVentasVend extends AppCompatActivity {
         ArrayAdapter adapter1= new ArrayAdapter(this,android.R.layout.simple_spinner_item,array_spin1);
         s1.setAdapter(adapter1);
         s1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 int sp1_pos=s1.getSelectedItemPosition();
@@ -55,25 +56,36 @@ public class TotVentasVend extends AppCompatActivity {
                     case 0:CasosR="M";
                         s2.setVisibility(View.VISIBLE);
                         s3.setVisibility(View.VISIBLE);
+                        btnStRg.setVisibility(View.GONE);
+
                         break;
                     case 1:CasosR="Y";
                         s2.setVisibility(View.GONE);
                         s3.setVisibility(View.VISIBLE);
+                        btnStRg.setVisibility(View.GONE);
 
                         break;
                     case 2:CasosR="R";
-                        cpv.setVisibility(View.VISIBLE);
+                        btnStRg.setVisibility(View.VISIBLE);
+                        btnStRg.setOnClickListener(new View.OnClickListener(){
 
+                            @Override
+                            public void onClick(View view) {
+                                smoothDateRangePickerFragment.show(getFragmentManager(), "smoothDateRangePicker");
+                            }
+                        });
                         break;
                 }
-                System.out.println(CasosR);
 
+
+                System.out.println(CasosR);
+                System.out.println(ranIni);
+                System.out.println(ranFin);
             }
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
-
 
         array_spin2 = new String [12];
         array_spin2[0]="Ene";
@@ -91,26 +103,55 @@ public class TotVentasVend extends AppCompatActivity {
         s2=(Spinner)findViewById(R.id.filt_M);
         ArrayAdapter adapter2= new ArrayAdapter(this,android.R.layout.simple_spinner_item,array_spin2);
         s2.setAdapter(adapter2);
+        s2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                int sp2_pos=s2.getSelectedItemPosition();
+
+                switch (sp2_pos){
+
+                }
 
 
+            }
 
-        array_spin3 = new String [8];
-        array_spin3[0]="2017";
-        array_spin3[1]="2016";
-        array_spin3[2]="2015";
-        array_spin3[3]="2014";
-        array_spin3[4]="2013";
-        array_spin3[5]="2012";
-        array_spin3[6]="2011";
-        array_spin3[7]="2010";
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        int anioBus=anioAct-2002;
+        array_spin3 = new String [anioBus];
+        for (int i=0;i<=anioBus;i++){
+            array_spin3[i]= String.valueOf(anioAct);
+            anioAct--;
+
+        }
 
 
         s3=(Spinner)findViewById(R.id.filt_Y);
         ArrayAdapter adapter= new ArrayAdapter(this,android.R.layout.simple_spinner_item,array_spin3);
         s3.setAdapter(adapter);
 
-
-
     }
+    SmoothDateRangePickerFragment smoothDateRangePickerFragment = SmoothDateRangePickerFragment.newInstance(
+            new SmoothDateRangePickerFragment.OnDateRangeSetListener() {
+
+                @Override
+                public void onDateRangeSet(SmoothDateRangePickerFragment view,
+                                           int yearStart, int monthStart,
+                                           int dayStart, int yearEnd,
+                                           int monthEnd, int dayEnd) {
+                    int mesIn=monthStart+1;
+                    int mesFn=monthEnd+1;
+
+                    ranIni=dayStart+"/"+mesIn+"/"+yearStart;
+                    ranFin=dayEnd+"/"+mesFn+"/"+yearEnd;
+
+                }
+
+            });
 
 }
